51a1cf30957ff758b40cd65014f8143eb6555612
unknown <ranjithkumar@gmail.com>
2025-07-31 15:02:22 +0530

    updated UhomeActivity

--- app/src/main/java/com/saveetha/foodstall/UhomeActivity.java
+++ app/src/main/java/com/saveetha/foodstall/UhomeActivity.java
@@ -1,24 +1,47 @@
 package com.saveetha.foodstall;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class UhomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.uhome);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.uhome); // Make sure this layout exists
+
+
+
+        // Set default selected item
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                Toast.makeText(this, "Home Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (itemId == R.id.nav_orders) {
+                startActivity(new Intent(this, OrdersActivity.class));
+                return true;
+            } else if (itemId == R.id.nav_wallet) {
+                startActivity(new Intent(this, WalletActivity.class));
+                return true;
+            } else if (itemId == R.id.nav_profile) {
+                startActivity(new Intent(this, ProfileActivity.class));
+                return true;
+            }
+
+            return false;
         });
     }
 }
